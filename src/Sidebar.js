612889b00e645eee04a5b44bf7f53f0ab8a3d17c
Mr. Blogger <siddh.mistry396321@gmail.com>
2021-11-30 23:45:37 +0530

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Sidebar.css'
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h1>Test is a testing</h1>
+            {/* Twitter Icon */}
+            <TwitterIcon />
+            {/* Sidebar icons */}
+            {/* Sidebar icons */}
+            {/* Sidebar icons */}
+            {/* Sidebar icons */}
+            {/* Sidebar icons */}
+            {/* Sidebar icons */}
+            {/* Tweet */}
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/SidebarOption.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebaroption() {
+    return (
+        <div className="sidebarOption">
+            
+        </div>
+    )
+}
+
+export default Sidebaroption
