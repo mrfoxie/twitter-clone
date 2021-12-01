947483984e473285520bcfd84e646b851f070486
Mr. Blogger <siddh.mistry396321@gmail.com>
2021-12-01 23:53:55 +0530

    file changes made

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -4,9 +4,9 @@ import {Avatar, Button} from '@material-ui/core';
 
 function TweetBox() {
     return (
-        <div classname="tweetBox">
+        <div className="tweetBox">
             <form>
-                <div className="tweetbox__input">
+                <div className="tweetBox__input">
 <Avatar src="https://www.hackeridiot.com/uploads/logo/logo_61278d3bb0b2a.png"></Avatar>
 <input placeholder="What's happening?" type="text" />
                 </div>
