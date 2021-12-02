63efeac81ac6527d0c730d0b749b0216bda3d1cd
Mr. Blogger <siddh.mistry396321@gmail.com>
2021-12-02 10:53:40 +0530

    Post added

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import './TweetBox.css';
-import {Avatar, Button} from '@material-ui/core';
+import React from "react";
+import "./TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
 
 function TweetBox() {
-    return (
-        <div className="tweetBox">
-            <form>
-                <div className="tweetBox__input">
-<Avatar src="https://www.hackeridiot.com/uploads/logo/logo_61278d3bb0b2a.png"></Avatar>
-<input placeholder="What's happening?" type="text" />
-                </div>
-                <Button>Tweet</Button>
-            </form>
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="https://www.hackeridiot.com/uploads/logo/logo_61278d3bb0b2a.png"></Avatar>
+          <input placeholder="What's happening?" type="text" />
         </div>
-    )
+        <input
+          className="tweetBox__inputImage"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+        <Button className="tweetBox__tweetButton">Tweet</Button>
+      </form>
+    </div>
+  );
 }
 
-export default TweetBox
+export default TweetBox;
